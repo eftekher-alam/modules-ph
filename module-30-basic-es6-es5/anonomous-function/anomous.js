0891fcd0fd8d37689eab5df373bbfadb07a055b9
eftekher-alam <eftekher@yahoo.com>
2023-08-27 19:12:17 +0600

    anonymous function

---
+++ module-30-basic-es6-es5/anonomous-function/anomous.js
@@ -0,0 +1,7 @@
+//Hence, we are not provide the name of the function so that it's called anonymous function 
+
+const fullname = function (fName, lName) {
+    return `${fName} ${lName}`;
+}
+
+console.log(fullname("Eftekher", "Shuvo"));
