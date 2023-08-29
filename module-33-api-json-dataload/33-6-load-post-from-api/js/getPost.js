4c1fb9bf30f6e14b14d71c50edc83563f1d1f9d2
eftekher-alam <eftekher@yahoo.com>
2023-08-29 10:33:31 +0600

    learn how to get data form API

---
+++ module-33-api-json-dataload/33-6-load-post-from-api/js/getPost.js
@@ -0,0 +1,18 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+fetch(url).then(res => res.json()).then(data => bind(data));
+
+function bind(data) {
+    const postContainer = document.querySelector("#post-container");
+    for (const post of data) {
+        const div = document.createElement("div");
+        div.style.border = "1px solid black";
+        div.style.borderRadius = "8px";
+        div.style.marginBottom = "16px";
+        const title = document.createElement("h3");
+        const postbody = document.createElement("p");
+        title.innerText = post.title;
+        postbody.innerText = post.body;
+        div.append(title, postbody);
+        postContainer.appendChild(div);
+    }
+}
