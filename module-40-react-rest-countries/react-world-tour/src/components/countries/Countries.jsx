9d0886c948dee9883bdd4b01173e559cc357467c
eftekher-alam <eftekher@yahoo.com>
2023-09-14 17:50:00 +0600

    Unsaved data to saved

--- module-40-react-rest-countries/react-world-tour/src/components/countries/Countries.jsx
+++ module-40-react-rest-countries/react-world-tour/src/components/countries/Countries.jsx
@@ -13,12 +13,35 @@ const Countries = () => {
             .then(data => setCountries(data));
     }, []);
     // console.log(countries[0]?.cca3);
+
+    const [visitedList, setVisitedList] = useState([]);
+
+    function handleVisitedCountry(country) {
+        const newVisitedList = [...visitedList, country?.name?.common];
+        setVisitedList(newVisitedList)
+    }
+
+    console.log("count visited : ", visitedList);
+
     return (
         <div>
-            <h2>Country Details</h2>
+            <h2>Country Details Here..!</h2>
+            <div>
+                <h3>Visited Country List</h3>
+                <ol>
+                    {
+                        //to ignore the error, key is passed.
+                        visitedList.map(name => <li key={name}>{name}</li>)
+                    }
+                </ol>
+            </div>
             <div className="grid">
                 {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        country={country}
+                        handleVisitedCountry={handleVisitedCountry}
+                    ></Country>)
                 }
             </div>
         </div>
--- module-40-react-rest-countries/react-world-tour/src/components/country/Country.jsx
+++ module-40-react-rest-countries/react-world-tour/src/components/country/Country.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "./Country.css"
 
-const Country = ({ country }) => {
-    console.log(country);
+const Country = ({ country, handleVisitedCountry }) => {
+    // console.log(country);
     const { name, flags } = country;
-    // console.log(flags);
+
+    const [isVisited, setVisited] = useState(false);
+
+    const handlerVisited = () => {
+        setVisited(true);
+        handleVisitedCountry(country);
+    }
+
     return (
-        <div>
+        <div className="country-container">
             <div className="flex">
                 <h3>Country : {name?.common}</h3>
                 <img src={flags?.png} className="flag" />
             </div>
+            <button className={isVisited ? "visited-button" : "default-button"} onClick={handlerVisited} >{isVisited ? "Visited" : "Visit"}</button>
         </div>
     );
 };
