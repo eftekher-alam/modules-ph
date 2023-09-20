2edc89108a0eb0db556ea1da1960d5e5242458f5
eftekher-alam <eftekher@yahoo.com>
2023-09-20 22:55:58 +0600

    Nested routing basic done

---
+++ module-45-react-router/react-nested-route/src/componentes/About/About.jsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+    return (
+        <div className="h-80 flex justify-center items-center">
+            <h1 className="text-4xl" >This is About Page</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ module-45-react-router/react-nested-route/src/componentes/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <div className="h-80 flex justify-center items-center">
+            <h1 className="text-4xl">This is Contact Page</h1>
+        </div>
+    );
+};
+
+export default Contact;
